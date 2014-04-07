2cb315abbd48d093f069ec796fd3129cf5be0690
ssw3131 <ssw3131@daum.net>
2014-04-07 15:40:42 +0900

    ajax

--- garnet.js
+++ garnet.js
@@ -1159,13 +1159,18 @@
 
             // TODO json parser
             // ajax
-            function ajax( $url, $cb, $dataType, $obj ){
-                var url = $url, cb = $cb, dt = $dataType, t0 = $obj.type || "GET", t1 = $obj.cache == undefined ? true : $obj.cache, t2 = $obj.postParam, req = getXHR();
+            function ajax( $url, $dataType, $obj ){
+//                var url = $url, cb = $cb, dt = $dataType, t0 = $obj.type || "GET", t1 = $obj.cache == undefined ? true : $obj.cache, t2 = $obj.postParam, req = getXHR();
+                var url = $url, dt = $dataType, r, t0 = $obj.type || "GET", t1 = $obj.cache == undefined ? true : $obj.cache, t2 = $obj.postParam, req = getXHR();
 
                 // XMLHttpRequest 상태변화
                 req.onreadystatechange = function(){
                     req.readyState == 4 ? req.status == 200
-                        ? cb( dt == "xml" ? req.responseXML : dt == "json" ? eval( "(" + req.responseText + ")" ) : dt == "text" ? req.responseText : null ) : null : null;
+                        ? r = dt == "xml" ? req.responseXML : req.responseText
+                        : null
+                    : null;
+                    trace( r );
+                    return r;
                 }
 
                 req.open( t0, url, true ), // XMLHttpRequest 연결
@@ -1213,19 +1218,20 @@
             Dk.loader = {
                 // text 로드
                 text : function( $url, $cb, $obj ){
-                    ajax( $url, $cb, "text", $obj ? $obj : {} );
+                    $cb( ajax( $url, "text", $obj ? $obj : {} ) );
                 },
 
                 // json 로드
                 json : function( $url, $cb, $obj ){
-                    ajax( $url, $cb, "json", $obj ? $obj : {} );
+//                    eval( "(" + req.responseText + ")" )
+                    $cb( ajax( $url, "json", $obj ? $obj : {} ) );
                 },
 
                 // xml 로드
                 xml : function( $url, $cb, $obj ){
-                    ajax( $url, function( $data ){
+                    $cb( ajax( $url, function( $data ){
                         xmlParser( $data, $cb )
-                    }, "xml", $obj ? $obj : {} );
+                    }, "xml", $obj ? $obj : {} ) );
                 },
 
                 // js 로드
