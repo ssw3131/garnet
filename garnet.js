2c4e3d94c0e3077b768002ea6e097ccd64b28ed8
ssw3131 <ssw3131@daum.net>
2014-04-07 15:10:03 +0900

    ajax

--- garnet.js
+++ garnet.js
@@ -1160,7 +1160,7 @@
 
             function jsonParse(text, reviver) {
 
-                var j;
+                var j, cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
 
                 function walk(holder, key) {
 
