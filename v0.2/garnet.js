abb07029cb721c589b5665dc8a7a31910027012a
Redcamel <webseon@gmail.com>
2014-08-22 13:28:35 +0900

    constructor 사용예제

--- v0.2/garnet.js
+++ v0.2/garnet.js
@@ -138,6 +138,7 @@
 			var t=dk.DETECTOR.currentTarget
 			return function dkEvent() {
 				var e=arguments[0]
+				this.name = 'dkEvent'
 				this.nativeEvent=e, this.keyCode=e.keyCode, this.target=e[t]
 				//TODO  여기다가 우리 이벤트를 정의합니다.
 				//TODO 사실 이건 팩토리가 도입되어 풀링처리를 해야됨
@@ -176,7 +177,7 @@
 // OBJS :
 		// 매니저 관련정의
 		fn.obj( 'KEY', (function() {
-			var r=dk.sList( 'KEY', 0 ), ev=dk.dkEvent, list=r.list, t0={},t3={},t1=("BACKSPACE,8,TAB,9,ENTER,13,SHIFT,16,CTRL,17,ALT,18,PAUSE,19,CAPSLOCK,20,ESC,27," + "PAGE_UP,33,PAGE_DOWN,34,END,35,HOME,36,LEFT_ARROW,37,UP_ARROW,38,RIGHT_ARROW,39,DOWN_ARROW,40,INSERT,45,DELETE,46," + "0,48,1,49,2,50,3,51,4,52,5,53,6,54,7,55,8,56,9,57,A,65,B,66,C,67,D,68,E,69,F,70,G,71,H,72,I,73,J,74,K,75,L,76,M,77,N,78,O,79,P,80,Q,81,R,82,S,83,T,84,U,85,V,86,W,87,X,88,Y,89,Z,90," + "NUMPAD_0,96,NUMPAD_1,97,NUMPAD_2,98,NUMPAD_3,99,NUMPAD_4,100,NUMPAD_5,101,NUMPAD_6,102,NUMPAD_7,103,NUMPAD_8,104,NUMPAD_9,105," + "'*',106,'+',107,'-',109,'.',110,'/',111,'=',187,COMA,188,'SLASH',191,'BACKSLASH',220," + "F1,112,F2,113,F3,114,F4,115,F5,116,F6,117,F7,118,F8,119,F9,120,F10,121,F11,122,F12,123").split( "," ), i=t1.length
+			var r=dk.sList( 'KEY', 0 ), ev=dk.dkEvent, list=r.list, t0={}, t3={}, t1=("BACKSPACE,8,TAB,9,ENTER,13,SHIFT,16,CTRL,17,ALT,18,PAUSE,19,CAPSLOCK,20,ESC,27," + "PAGE_UP,33,PAGE_DOWN,34,END,35,HOME,36,LEFT_ARROW,37,UP_ARROW,38,RIGHT_ARROW,39,DOWN_ARROW,40,INSERT,45,DELETE,46," + "0,48,1,49,2,50,3,51,4,52,5,53,6,54,7,55,8,56,9,57,A,65,B,66,C,67,D,68,E,69,F,70,G,71,H,72,I,73,J,74,K,75,L,76,M,77,N,78,O,79,P,80,Q,81,R,82,S,83,T,84,U,85,V,86,W,87,X,88,Y,89,Z,90," + "NUMPAD_0,96,NUMPAD_1,97,NUMPAD_2,98,NUMPAD_3,99,NUMPAD_4,100,NUMPAD_5,101,NUMPAD_6,102,NUMPAD_7,103,NUMPAD_8,104,NUMPAD_9,105," + "'*',106,'+',107,'-',109,'.',110,'/',111,'=',187,COMA,188,'SLASH',191,'BACKSLASH',220," + "F1,112,F2,113,F3,114,F4,115,F5,116,F6,117,F7,118,F8,119,F9,120,F10,121,F11,122,F12,123").split( "," ), i=t1.length
 			while( i-- ) t3[t1[i--]]=t1[i].toLowerCase(), t0[t1[i].toLowerCase()]=0
 			dk.addEvent( W, 'keydown', function() {
 					var t=new ev( arguments[0] )
