6da071fc2fa07febbe012f6b371345b8c25293a4
sewon <ssw3131@daum.net>
2014-08-27 19:23:09 +0900

    dk.Dom
    2 생성인자
      # element 문자열 생성
    
    3 fn 프로토타입 외부설정
    
    API
    	생성
    	// text
    	dk.Dom( '<div></div>' );
    	dk.Dom( '<div' );
    	dk.Dom();
    	// selector
    	dk.Dom( '#test' );
    	// element
    	dk.Dom( document.createElement( 'div' ) );
    	// cache
    	dk.Dom( '#test', null );
    
    	프로토타입 함수
    	dk.Dom.fn( $k, $v )

--- v0.2/garnet.js
+++ v0.2/garnet.js
@@ -2,7 +2,7 @@
 (function(){
 	'use strict';
 	var W = window, DOC = document, HEAD = DOC.getElementsByTagName( 'head' )[0];
-	var dk, fn, query, dkEvent;
+	var dk, fn, selector, dkEvent;
 	var trim = /^\s*|\s*$/g;
 
 // 보정패치 :
@@ -24,7 +24,7 @@
 			function sMethod(){};
 			sMethod.prototype = {
 				S : function(){
-					var i = 0, j = arguments.length, k, v; //루프용 i,j와 키밸류용 k, v
+					var i = 0, j = arguments.length, k, v;
 					while( i < j ){
 						k = arguments[i++];
 						if( i == j ){
@@ -37,7 +37,7 @@
 							typeof this[k] == 'function' ? this[k]( v ) : this[k] = v
 						}
 					}
-					return this; //TODO 재질은 this로가고 메쉬류는 v로가야하는겐가!!
+					return this;
 				}
 			}
 		})(),
@@ -54,17 +54,124 @@
 						return
 				}
 				if( DOC && DOC.getElementsByTagName && DOC.getElementById && DOC.body && DOC.readyState ) clearInterval( check ), $host ? $host() : null
-			}, 1 );
+			}, 10 );
 		},
 
 // CORE :
-		dk.method = fn = function( $k, $v ){ dk[ $k.charAt( 0 ).toLowerCase() + $k.substring( 1, $k.length ) ] = $v },
+		dk.fn = fn = function( $k, $v ){ dk[ $k.charAt( 0 ).toLowerCase() + $k.substring( 1, $k.length ) ] = $v },
 		dk.class = function( $k, $v ){ $k = $k.replace( trim, '' ).toLowerCase(), dk[ $k.charAt( 0 ).toUpperCase() + $k.substring( 1, $k.length ) ] = $v },
 		dk.static = function( $k, $v ){ dk[ $k.replace( trim, '' ).toUpperCase() ] = $v },
 
+// INFO :
+		dk.static( 'INFO', { name : "Dk garnet", version : "v0.2.0", github : "https://github.com/ssw3131/garnet.git" } ),
+
+// DETECTOR :
+		dk.static( 'DETECTOR', (function(){
+			var navi = W.navigator, agent = navi.userAgent.toLowerCase(), platform = navi.platform.toLowerCase(), app = navi.appVersion.toLowerCase(),
+				device = 'pc', os, osv, browser, bv, flash,
+				prefixCss, prefixStyle, transform3D, keyframe = W['CSSRule'],
+				docMode = 0,
+				d = DOC.createElement( 'div' ), s = d.style, c = DOC.createElement( 'canvas' ), a = DOC.createElement( 'audio' ), v = DOC.createElement( 'video' ), t0,
+				ie = function(){
+					if( agent.indexOf( 'msie' ) < 0 && agent.indexOf( 'trident' ) < 0 ) return;
+					if( agent.indexOf( 'iemobile' ) > -1 ) os = 'winMobile';
+					return browser = 'ie', bv = agent.indexOf( 'msie 7' ) > -1 && agent.indexOf( 'trident' ) > -1 ? -1 : agent.indexOf( 'msie' ) < 0 ? 11 : parseFloat( /msie ([\d]+)/.exec( agent )[1] );
+				},
+				chrome = function(){
+					if( agent.indexOf( t0 = 'chrome' ) < 0 && agent.indexOf( t0 = 'crios' ) < 0 ) return;
+					return browser = 'chrome', bv = parseFloat( ( t0 == 'chrome' ? /chrome\/([\d]+)/ : /crios\/([\d]+)/ ).exec( agent )[1] );
+				},
+				firefox = function(){ return agent.indexOf( 'firefox' ) < 0 ? 0 : ( browser = 'firefox', bv = parseFloat( /firefox\/([\d]+)/.exec( agent )[1] ) ); },
+				safari = function(){ return agent.indexOf( 'safari' ) < 0 ? 0 : ( browser = 'safari', bv = parseFloat( /safari\/([\d]+)/.exec( agent )[1] ) ); },
+				opera = function(){
+					var i;
+					return ( agent.indexOf( i = 'opera' ) < 0 && agent.indexOf( i = 'opr' ) < 0 ) ? 0 : ( browser = 'opera', bv = ( i == 'opera' ) ? parseFloat( /version\/([\d]+)/.exec( agent )[1] ) : parseFloat( /opr\/([\d]+)/.exec( agent )[1] ) );
+				},
+				naver = function(){ return agent.indexOf( 'naver' ) < 0 ? 0 : browser = 'naver'; };
+
+			// win
+			if( agent.indexOf( 'android' ) > -1 ){
+				browser = os = 'android';
+				if( agent.indexOf( 'mobile' ) == -1 ) browser += 'Tablet', device = 'tablet';
+				else device = 'mobile';
+				if( t0 = /android ([\d.]+)/.exec( agent ) ) t0 = t0[1].split( '.' ), osv = parseFloat( t0[0] + '.' + t0[1] );
+				else osv = 0;
+				if( t0 = /safari\/([\d.]+)/.exec( agent ) ) bv = parseFloat( t0[1] );
+				naver() || chrome() || opera() || firefox();
+			}else if( agent.indexOf( t0 = 'ipad' ) > -1 || agent.indexOf( t0 = 'iphone' ) > -1 ){
+				device = t0 == 'ipad' ? 'tablet' : 'mobile', browser = os = t0;
+				if( t0 = /os ([\d_]+)/.exec( agent ) ) t0 = t0[1].split( '_' ), osv = parseFloat( t0[0] + '.' + t0[1] );
+				else osv = 0;
+				if( t0 = /mobile\/([\S]+)/.exec( agent ) ) bv = parseFloat( t0[1] );
+				naver() || chrome() || opera() || firefox();
+			}else{
+				if( platform.indexOf( 'win' ) > -1 ){
+					os = 'win', t0 = 'windows nt ';
+					if( agent.indexOf( t0 + '5.1' ) > -1 ) osv = 'xp';
+					else if( agent.indexOf( t0 + '6.0' ) > -1 ) osv = 'vista';
+					else if( agent.indexOf( t0 + '6.1' ) > -1 ) osv = '7';
+					else if( agent.indexOf( t0 + '6.2' ) > -1 ) osv = '8';
+					else if( agent.indexOf( t0 + '6.3' ) > -1 ) osv = '8.1';
+					ie() || chrome() || firefox() || safari() || opera();
+				}else if( platform.indexOf( 'mac' ) > -1 ){
+					os = 'mac', t0 = /os x ([\d._]+)/.exec( agent )[1].replace( '_', '.' ).split( '.' ), osv = parseFloat( t0[0] + '.' + t0[1] ),
+						safari() || chrome() || firefox() || opera();
+				}else{
+					os = app.indexOf( 'x11' ) > -1 ? 'unix' : app.indexOf( 'linux' ) > -1 ? 'linux' : 0, chrome() || firefox();
+				}
+			}
+			(function(){
+				var plug = navi.plugins, t0;
+				if( browser == 'ie' ) try{ t0 = new ActiveXObject( 'ShockwaveFlash.ShockwaveFlash' ).GetVariable( '$version' ).substr( 4 ).split( ',' ), flash = parseFloat( t0[0] + '.' + t0[1] ); }catch( e ){ }
+				else if( ( t0 = plug['Shockwave Flash 2.0'] ) || ( t0 = plug['Shockwave Flash'] ) ) t0 = t0.description.split( ' ' )[2].split( '.' ), flash = parseFloat( t0[0] + '.' + t0[1] );
+				else if( agent.indexOf( 'webtv' ) > -1 ) flash = agent.indexOf( 'webtv/2.6' ) > -1 ? 4 : agent.indexOf( "webtv/2.5" ) > -1 ? 3 : 2;
+			})();
+			// dom
+			switch( browser ){
+				case'ie':
+					if( bv == -1 ) bv = !c['getContext'] ? 8 : !( 'msTransition' in s ) && !( 'transition' in s ) ? 9 : c.getContext( 'webgl' ) || c.getContext( 'experimental-webgl' ) ? 11 : 10;
+					prefixCss = '-ms-', prefixStyle = 'ms', transform3D = bv > 9 ? 1 : 0, docMode = DOC['documentMode'] || 0;
+					break;
+				case'firefox':
+					prefixCss = '-moz-', prefixStyle = 'Moz', transform3D = 1;
+					break;
+				case'opera':
+					prefixCss = '-o-', prefixStyle = 'O', transform3D = 1;
+					break;
+				default:
+					prefixCss = '-webkit-', prefixStyle = 'webkit', transform3D = os == 'android' ? ( osv < 4 ? 0 : 1 ) : 1;
+			}
+			if( keyframe ){
+				if( keyframe.WEBKIT_KEYFRAME_RULE ) keyframe = '-webkit-keyframes';
+				else if( keyframe.MOZ_KEYFRAME_RULE ) keyframe = '-moz-keyframes';
+				else if( keyframe.KEYFRAME_RULE ) keyframe = 'keyframes';
+				else keyframe = null;
+			}
+			return {
+				device : device, browser : browser, browserVer : bv, os : os, osVer : osv,
+				mobile : device == 'pc' ? 0 : 1,
+				flash : flash,
+				prefixCss : prefixCss, prefixStyle : prefixStyle,
+				transform3D : transform3D, transform : ( prefixStyle + 'Transform' in s || 'transform' in s ) ? 1 : 0, transition : ( prefixStyle + 'Transition' in s || 'transition' in s ) ? 1 : 0,
+				keyframe : keyframe ? 1 : 0,
+				canvas : c ? 1 : 0, canvasText : c && c['getContext'] && c.getContext( '2d' ).fillText ? 1 : 0,
+				audio : a ? 1 : 0, video : v ? 1 : 0,
+				videoPoster : v && 'poster' in v ? 1 : 0,
+				videoWebm : v && v[ 'canPlayType' ] && v.canPlayType( 'video/webm; codecs="vp8,mp4a.40.2"' ).indexOf( 'no' ) == -1 ? 1 : 0,
+				videoH264 : v && v[ 'canPlayType' ] && v.canPlayType( 'video/mp4; codecs="avc1.4D401E, mp4a.40.2"' ).indexOf( 'no' ) == -1 ? 1 : 0,
+				videoTeora : v && v[ 'canPlayType' ] && v.canPlayType( 'video/ogg; codecs="theora,vorbis"' ).indexOf( 'no' ) == -1 ? 1 : 0,
+				insertBefore : 'insertBefore' in d ? 1 : 0,
+				innerText : 'innerText' in d ? 1 : 0, textContent : 'textContent' in d ? 1 : 0,
+				touchBool : 'ontouchstart' in W ? 1 : 0,
+				currentTarget : browser == "firefox" ? "target" : "srcElement",
+				wheelEvent : browser == "firefox" ? "DOMMouseScroll" : "mousewheel",
+				isLocalhost : location.host.indexOf( "localhost" ) < 0 ? 0 : 1
+			}
+		})() ),
+
 // SELECTOR :
-		dk.query = query = (function(){
-			var bsSelector = function( doc, trim, domData ){
+		fn( 'selector', selector = (function(){
+			function bsSelector( doc, trim, domData ){
 				'use strict';
 				var compare = {
 						// id
@@ -410,110 +517,6 @@
 				};
 			};
 			return bsSelector( document, /^\s*|\s*$/g );
-		})(),
-
-// DETECTOR :
-		dk.static( 'DETECTOR', (function(){
-			var navi = W.navigator, agent = navi.userAgent.toLowerCase(), platform = navi.platform.toLowerCase(), app = navi.appVersion.toLowerCase(),
-				device = 'pc', os, osv, browser, bv, flash,
-				prefixCss, prefixStyle, transform3D, keyframe = W['CSSRule'],
-				docMode = 0,
-				d = DOC.createElement( 'div' ), s = d.style, c = DOC.createElement( 'canvas' ), a = DOC.createElement( 'audio' ), v = DOC.createElement( 'video' ), t0,
-				ie = function(){
-					if( agent.indexOf( 'msie' ) < 0 && agent.indexOf( 'trident' ) < 0 ) return;
-					if( agent.indexOf( 'iemobile' ) > -1 ) os = 'winMobile';
-					return browser = 'ie', bv = agent.indexOf( 'msie 7' ) > -1 && agent.indexOf( 'trident' ) > -1 ? -1 : agent.indexOf( 'msie' ) < 0 ? 11 : parseFloat( /msie ([\d]+)/.exec( agent )[1] );
-				},
-				chrome = function(){
-					if( agent.indexOf( t0 = 'chrome' ) < 0 && agent.indexOf( t0 = 'crios' ) < 0 ) return;
-					return browser = 'chrome', bv = parseFloat( ( t0 == 'chrome' ? /chrome\/([\d]+)/ : /crios\/([\d]+)/ ).exec( agent )[1] );
-				},
-				firefox = function(){ return agent.indexOf( 'firefox' ) < 0 ? 0 : ( browser = 'firefox', bv = parseFloat( /firefox\/([\d]+)/.exec( agent )[1] ) ); },
-				safari = function(){ return agent.indexOf( 'safari' ) < 0 ? 0 : ( browser = 'safari', bv = parseFloat( /safari\/([\d]+)/.exec( agent )[1] ) ); },
-				opera = function(){
-					var i;
-					return ( agent.indexOf( i = 'opera' ) < 0 && agent.indexOf( i = 'opr' ) < 0 ) ? 0 : ( browser = 'opera', bv = ( i == 'opera' ) ? parseFloat( /version\/([\d]+)/.exec( agent )[1] ) : parseFloat( /opr\/([\d]+)/.exec( agent )[1] ) );
-				},
-				naver = function(){ return agent.indexOf( 'naver' ) < 0 ? 0 : browser = 'naver'; };
-
-			// win
-			if( agent.indexOf( 'android' ) > -1 ){
-				browser = os = 'android';
-				if( agent.indexOf( 'mobile' ) == -1 ) browser += 'Tablet', device = 'tablet';
-				else device = 'mobile';
-				if( t0 = /android ([\d.]+)/.exec( agent ) ) t0 = t0[1].split( '.' ), osv = parseFloat( t0[0] + '.' + t0[1] );
-				else osv = 0;
-				if( t0 = /safari\/([\d.]+)/.exec( agent ) ) bv = parseFloat( t0[1] );
-				naver() || chrome() || opera() || firefox();
-			}else if( agent.indexOf( t0 = 'ipad' ) > -1 || agent.indexOf( t0 = 'iphone' ) > -1 ){
-				device = t0 == 'ipad' ? 'tablet' : 'mobile', browser = os = t0;
-				if( t0 = /os ([\d_]+)/.exec( agent ) ) t0 = t0[1].split( '_' ), osv = parseFloat( t0[0] + '.' + t0[1] );
-				else osv = 0;
-				if( t0 = /mobile\/([\S]+)/.exec( agent ) ) bv = parseFloat( t0[1] );
-				naver() || chrome() || opera() || firefox();
-			}else{
-				if( platform.indexOf( 'win' ) > -1 ){
-					os = 'win', t0 = 'windows nt ';
-					if( agent.indexOf( t0 + '5.1' ) > -1 ) osv = 'xp';
-					else if( agent.indexOf( t0 + '6.0' ) > -1 ) osv = 'vista';
-					else if( agent.indexOf( t0 + '6.1' ) > -1 ) osv = '7';
-					else if( agent.indexOf( t0 + '6.2' ) > -1 ) osv = '8';
-					else if( agent.indexOf( t0 + '6.3' ) > -1 ) osv = '8.1';
-					ie() || chrome() || firefox() || safari() || opera();
-				}else if( platform.indexOf( 'mac' ) > -1 ){
-					os = 'mac', t0 = /os x ([\d._]+)/.exec( agent )[1].replace( '_', '.' ).split( '.' ), osv = parseFloat( t0[0] + '.' + t0[1] ),
-						safari() || chrome() || firefox() || opera();
-				}else{
-					os = app.indexOf( 'x11' ) > -1 ? 'unix' : app.indexOf( 'linux' ) > -1 ? 'linux' : 0, chrome() || firefox();
-				}
-			}
-			(function(){
-				var plug = navi.plugins, t0;
-				if( browser == 'ie' ) try{ t0 = new ActiveXObject( 'ShockwaveFlash.ShockwaveFlash' ).GetVariable( '$version' ).substr( 4 ).split( ',' ), flash = parseFloat( t0[0] + '.' + t0[1] ); }catch( e ){ }
-				else if( ( t0 = plug['Shockwave Flash 2.0'] ) || ( t0 = plug['Shockwave Flash'] ) ) t0 = t0.description.split( ' ' )[2].split( '.' ), flash = parseFloat( t0[0] + '.' + t0[1] );
-				else if( agent.indexOf( 'webtv' ) > -1 ) flash = agent.indexOf( 'webtv/2.6' ) > -1 ? 4 : agent.indexOf( "webtv/2.5" ) > -1 ? 3 : 2;
-			})();
-			// dom
-			switch( browser ){
-				case'ie':
-					if( bv == -1 ) bv = !c['getContext'] ? 8 : !( 'msTransition' in s ) && !( 'transition' in s ) ? 9 : c.getContext( 'webgl' ) || c.getContext( 'experimental-webgl' ) ? 11 : 10;
-					prefixCss = '-ms-', prefixStyle = 'ms', transform3D = bv > 9 ? 1 : 0, docMode = DOC['documentMode'] || 0;
-					break;
-				case'firefox':
-					prefixCss = '-moz-', prefixStyle = 'Moz', transform3D = 1;
-					break;
-				case'opera':
-					prefixCss = '-o-', prefixStyle = 'O', transform3D = 1;
-					break;
-				default:
-					prefixCss = '-webkit-', prefixStyle = 'webkit', transform3D = os == 'android' ? ( osv < 4 ? 0 : 1 ) : 1;
-			}
-			if( keyframe ){
-				if( keyframe.WEBKIT_KEYFRAME_RULE ) keyframe = '-webkit-keyframes';
-				else if( keyframe.MOZ_KEYFRAME_RULE ) keyframe = '-moz-keyframes';
-				else if( keyframe.KEYFRAME_RULE ) keyframe = 'keyframes';
-				else keyframe = null;
-			}
-			return {
-				device : device, browser : browser, browserVer : bv, os : os, osVer : osv,
-				mobile : device == 'pc' ? 0 : 1,
-				flash : flash,
-				prefixCss : prefixCss, prefixStyle : prefixStyle,
-				transform3D : transform3D, transform : ( prefixStyle + 'Transform' in s || 'transform' in s ) ? 1 : 0, transition : ( prefixStyle + 'Transition' in s || 'transition' in s ) ? 1 : 0,
-				keyframe : keyframe ? 1 : 0,
-				canvas : c ? 1 : 0, canvasText : c && c['getContext'] && c.getContext( '2d' ).fillText ? 1 : 0,
-				audio : a ? 1 : 0, video : v ? 1 : 0,
-				videoPoster : v && 'poster' in v ? 1 : 0,
-				videoWebm : v && v[ 'canPlayType' ] && v.canPlayType( 'video/webm; codecs="vp8,mp4a.40.2"' ).indexOf( 'no' ) == -1 ? 1 : 0,
-				videoH264 : v && v[ 'canPlayType' ] && v.canPlayType( 'video/mp4; codecs="avc1.4D401E, mp4a.40.2"' ).indexOf( 'no' ) == -1 ? 1 : 0,
-				videoTeora : v && v[ 'canPlayType' ] && v.canPlayType( 'video/ogg; codecs="theora,vorbis"' ).indexOf( 'no' ) == -1 ? 1 : 0,
-				insertBefore : 'insertBefore' in d ? 1 : 0,
-				innerText : 'innerText' in d ? 1 : 0, textContent : 'textContent' in d ? 1 : 0,
-				touchBool : 'ontouchstart' in W ? 1 : 0,
-				currentTarget : browser == "firefox" ? "target" : "srcElement",
-				wheelEvent : browser == "firefox" ? "DOMMouseScroll" : "mousewheel",
-				isLocalhost : location.host.indexOf( "localhost" ) < 0 ? 0 : 1
-			}
 		})() ),
 
 // EVENT :
@@ -713,6 +716,11 @@
 				self.el = $e, self.style = s;
 			}
 
+			function destroyDom( $k ){
+				// todo 돔트리 제거, 이벤트 제거
+				delete uuList[ $k ];
+			}
+
 			function listDom( $arr ){
 				var r = [], i = $arr.length;
 				if( i == 1 ) r = new Dom( $arr[ --i ] );
@@ -720,18 +728,33 @@
 				return r;
 			}
 
-			function destroyDom( $k ){
-				// todo 돔트리 제거, 이벤트 제거
-				delete uuList[ $k ];
+			function parser( $str ){
+				var t0;
+				if( $str.indexOf( '>' ) < 0 ) return DOC.createElement( $str.substring( 1, $str.length ) );
+				else return t0 = DOC.createElement( 'div' ), t0.innerHTML = $str, t0.firstChild;
 			}
 
-			return function( $k, $v ){
-				if( typeof $k === 'string' ){ // 문자열
+			function factory( $k, $v ){
+				if( $k === undefined ){
+					return new Dom( DOC.createElement( 'div' ) );
+				}else if( typeof $k === 'string' ){ // 문자열
+					if( $k.charAt( 0 ) == '<' ) return new Dom( parser( $k ) ); // 태그
 					if( $v === null ) return destroyDom( $k ); // 돔제거
 					return uuList[ $k ] ? uuList[ $k ] : uuList[ $k ] = listDom( query( $k ) ); // 캐싱, 쿼리
 				}else{ // element
 					return $k.length ? listDom( $k ) : new Dom( $k );
 				}
 			}
+
+			factory.fn = function(){
+				var i = 0, j = arguments.length, k, v, fn = Dom.prototype;
+				while( i < j ){
+					k = arguments[ i++ ];
+					if( i == j ) return fn[ k ];
+					else v = arguments[ i++ ], v === null ? delete fn[ k ] : fn[ k ] = v;
+				}
+			}
+
+			return factory;
 		})() );
 })();
