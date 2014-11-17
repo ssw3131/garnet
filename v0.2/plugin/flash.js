b006da277ff6a860ef8d32899d166ac2312ba5c0
sewon <ssw3131@daum.net>
2014-11-17 20:13:21 +0900

    plugin flash

--- v0.2/plugin/flash.js
+++ v0.2/plugin/flash.js
@@ -111,6 +111,7 @@
 					this.el.removeChild( this.conEl );
 				},
 				toFlash : function( $v ){
+					// todo ie10 이하 toFlash 문제
 					var f, t0;
 					$v = $v.replace( /(\s*)/g, "" ),
 						f = ( t0 = $v.split( '(' ) )[ 0 ],
