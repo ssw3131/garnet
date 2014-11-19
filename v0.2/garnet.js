9b45d91bf0111c95f7cd023cdee686ced7163c4a
sewon <ssw3131@daum.net>
2014-11-19 10:55:10 +0900

    141119

--- v0.2/garnet.js
+++ v0.2/garnet.js
@@ -731,9 +731,9 @@
 							k = arguments[ i++ ];
 							if( i == j ) return proto[ k ] ? proto[ k ].call( { style : s } ) :
 								( r = s[ k ], t0 = parseFloat( r ), r = isNaN( t0 ) ? r : t0 );
-							else  v = arguments[ i++ ], log( v ), log( k ),
+							else  v = arguments[ i++ ], alert( v ), alert( k ),
 								proto[ k ] ? proto[ k ].call( { style : s }, v ) :
-									s[ k ] = s[ prefixCss + k ] = typeof v == 'number' ? nopx[ k ] ? v : v + 'px' : v, log( s ), log( k )
+									s[ k ] = s[ prefixCss + k ] = typeof v == 'number' ? nopx[ k ] ? v : v + 'px' : v, alert( s ), alert( k )
 						}
 						return this;
 					}
