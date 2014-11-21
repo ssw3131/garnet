080f4d6dff27a1cf5d33ebd0a3e1ab6a3cb2e1b5
sewon <ssw3131@daum.net>
2014-11-21 16:37:11 +0900

    141121

--- v0.2/garnet.js
+++ v0.2/garnet.js
@@ -842,19 +842,18 @@
 		dk.fn( 'sList', (function(){
 			function dkList(){
 				var i, j, t;
-				this.list = {}, this._list = [], this.name = arguments[0]
-				this.update = arguments[1] ? function(){
-					t = this._list, i = t.length, j = i % 8
-					while( i-- > j ) t[i--](), t[i--](), t[i--](), t[i--](), t[i--](), t[i--](), t[i--](), t[i]()
-					while( j-- ) t[j]()
-				} : 0
+				this.list = {}, this._list = [], this.name = arguments[ 0 ],
+					this.update = arguments[ 1 ] ? function(){
+						t = this._list, i = t.length, j = i % 8
+						while( i-- > j ) t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i ]();
+						while( j-- ) t[ j ]();
+					} : 0;
 			}
 
 			function reset(){
-				var k, t0 = arguments[0], t1 = t0.list, t2 = t0._list
-				t2 = []
-				for( k in t1 ) t2.push( t1[k] )
-				t0._list = t2
+				var k, t0 = this.list, t1 = [];
+				for( k in t0 ) t1.push( t0[ k ] );
+				this._list = t1;
 			}
 
 			dkList.prototype = {
@@ -863,27 +862,24 @@
 					while( i < j ){
 						k = arguments[i++];
 						if( i == j ){
-							if( k == 'this' ) return this;
-							else if( k == 'list' ) return this.list;
-							return typeof this[k] == 'function' ? this[k]() : this.list[k]
-						}
-						else{
-							v = arguments[i++]
-							if( v === null ) delete this[k];
-							typeof this[k] == 'function' ? this[k]( v ) : this.list[k] = v
+							if( k == 'list' ) return this.list;
+							return this.list[ k ];
+						}else{
+							v = arguments[ i++ ];
+							if( v === null ) delete this[ k ];
+							this.list[ k ] = v;
 						}
 					}
-					reset( this )
+					reset.call( this );
 					return v;
 				}
 			}
-			return function( k, update ){
-				return new dkList( k, update )
+			return function( $k, $update ){
+				return new dkList( $k, $update );
 			}
 		})() ),
 		dk.obj( 'KEY', (function(){
 			var r = dk.sList( 'KEY', 0 ), ev = dkEvent, list = r.list, t0 = {}, t3 = {}, t1 = ( "BACKSPACE,8,TAB,9,ENTER,13,SHIFT,16,CTRL,17,ALT,18,PAUSE,19,CAPSLOCK,20,ESC,27," + "PAGE_UP,33,PAGE_DOWN,34,END,35,HOME,36,LEFT_ARROW,37,UP_ARROW,38,RIGHT_ARROW,39,DOWN_ARROW,40,INSERT,45,DELETE,46," + "0,48,1,49,2,50,3,51,4,52,5,53,6,54,7,55,8,56,9,57,A,65,B,66,C,67,D,68,E,69,F,70,G,71,H,72,I,73,J,74,K,75,L,76,M,77,N,78,O,79,P,80,Q,81,R,82,S,83,T,84,U,85,V,86,W,87,X,88,Y,89,Z,90," + "NUMPAD_0,96,NUMPAD_1,97,NUMPAD_2,98,NUMPAD_3,99,NUMPAD_4,100,NUMPAD_5,101,NUMPAD_6,102,NUMPAD_7,103,NUMPAD_8,104,NUMPAD_9,105," + "'*',106,'+',107,'-',109,'.',110,'/',111,'=',187,COMA,188,'SLASH',191,'BACKSLASH',220," + "F1,112,F2,113,F3,114,F4,115,F5,116,F6,117,F7,118,F8,119,F9,120,F10,121,F11,122,F12,123" ).split( "," ), i = t1.length;
-//			log( t1 );
 			while( i-- ) t3[ t1[ i-- ] ] = t1[ i ].toLowerCase(), t0[ t1[ i ].toLowerCase() ] = 0;
 			dk.addEvent( W, 'keydown', function( $e ){
 					var t = ev( $e );
