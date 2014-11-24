2691c5ea01eb5812c8dec3c36f66235002ef2ad8
sewon <ssw3131@daum.net>
2014-11-24 18:45:43 +0900

    sList

--- v0.2/garnet.js
+++ v0.2/garnet.js
@@ -841,14 +841,13 @@
 
 // OBJ :
 		dk.fn( 'sList', (function(){
-			function dkList(){
-				var i, j, t;
-				this.list = {}, this._list = [], this.name = arguments[ 0 ],
-					this.update = arguments[ 1 ] ? function(){
+			function dkList( $k, $update ){
+				this.list = {}, this._list = [], this.name = $k,
+					this.update = $update ? function( $param ){
+						var t, i, j;
 						t = this._list, i = t.length, j = i % 8;
-//						while( i-- > j ) t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i-- ](), t[ i ]();
-//						while( j-- ) t[ j ]();
-						while( i-- ) t[ i ]( arguments[ 0 ] );
+						while( i-- > j ) t[ i-- ]( $param ), t[ i-- ]( $param ), t[ i-- ]( $param ), t[ i-- ]( $param ), t[ i-- ]( $param ), t[ i-- ]( $param ), t[ i-- ]( $param ), t[ i ]( $param );
+						while( j-- ) t[ j ]( $param );
 					} : 0;
 			}
 
@@ -863,14 +862,8 @@
 					var i = 0, j = arguments.length, k, v;
 					while( i < j ){
 						k = arguments[i++];
-						if( i == j ){
-							if( k == 'list' ) return this.list;
-							return this.list[ k ];
-						}else{
-							v = arguments[ i++ ];
-							if( v === null ) delete this[ k ];
-							this.list[ k ] = v;
-						}
+						if( i == j ) return this.list[ k ];
+						else v = arguments[ i++ ], v === null ? delete this.list[ k ] : this.list[ k ] = v;
 					}
 					reset.call( this );
 					return v;
@@ -889,15 +882,15 @@
 			return r;
 		})( dk.sList ) ),
 
-		dk.obj( 'KEY', (function( $sList, $dkEvent ){
-			var r = $sList( 'KEY', 0 ), list = r.list, t0 = {}, t1 = {}, t2 = ( "BACKSPACE,8,TAB,9,ENTER,13,SHIFT,16,CTRL,17,ALT,18,PAUSE,19,CAPSLOCK,20,ESC,27," + "PAGE_UP,33,PAGE_DOWN,34,END,35,HOME,36,LEFT_ARROW,37,UP_ARROW,38,RIGHT_ARROW,39,DOWN_ARROW,40,INSERT,45,DELETE,46," + "0,48,1,49,2,50,3,51,4,52,5,53,6,54,7,55,8,56,9,57,A,65,B,66,C,67,D,68,E,69,F,70,G,71,H,72,I,73,J,74,K,75,L,76,M,77,N,78,O,79,P,80,Q,81,R,82,S,83,T,84,U,85,V,86,W,87,X,88,Y,89,Z,90," + "NUMPAD_0,96,NUMPAD_1,97,NUMPAD_2,98,NUMPAD_3,99,NUMPAD_4,100,NUMPAD_5,101,NUMPAD_6,102,NUMPAD_7,103,NUMPAD_8,104,NUMPAD_9,105," + "'*',106,'+',107,'-',109,'.',110,'/',111,'=',187,COMA,188,'SLASH',191,'BACKSLASH',220," + "F1,112,F2,113,F3,114,F4,115,F5,116,F6,117,F7,118,F8,119,F9,120,F10,121,F11,122,F12,123" ).split( "," ), i = t2.length;
+		dk.obj( 'KEY', (function( $sList, $addEvent, $dkEvent ){
+			var r = $sList( 'KEY', 0 ), list = r.list, t0 = {}, t1 = {}, t2 = ( "BACKSPACE,8,TAB,9,ENTER,13,SHIFT,16,CTRL,17,ALT,18,PAUSE,19,CAPSLOCK,20,ESC,27," + "PAGE_UP,33,PAGE_DOWN,34,END,35,HOME,36,LEFT_ARROW,37,UP_ARROW,38,RIGHT_ARROW,39,DOWN_ARROW,40,INSERT,45,DELETE,46,NUMLOCK,144,SCROLLLOCK,145," + "0,48,1,49,2,50,3,51,4,52,5,53,6,54,7,55,8,56,9,57,A,65,B,66,C,67,D,68,E,69,F,70,G,71,H,72,I,73,J,74,K,75,L,76,M,77,N,78,O,79,P,80,Q,81,R,82,S,83,T,84,U,85,V,86,W,87,X,88,Y,89,Z,90," + "NUMPAD_0,96,NUMPAD_1,97,NUMPAD_2,98,NUMPAD_3,99,NUMPAD_4,100,NUMPAD_5,101,NUMPAD_6,102,NUMPAD_7,103,NUMPAD_8,104,NUMPAD_9,105," + "'*',106,'+',107,'-',109,'.',110,'/',111,'=',187,COMA,188,'SLASH',191,'BACKSLASH',220," + "F1,112,F2,113,F3,114,F4,115,F5,116,F6,117,F7,118,F8,119,F9,120,F10,121,F11,122,F12,123" ).split( "," ), i = t2.length;
 			while( i-- ) t1[ t2[ i-- ] ] = t2[ i ].toLowerCase(), t0[ t2[ i ].toLowerCase() ] = 0;
-			dk.addEvent( W, 'keydown', function( $e ){
+			$addEvent( W, 'keydown', function( $e ){
 				var ev = $dkEvent( $e ), t0 = list[ t1[ ev.keyCode = $e.keyCode ] ];
 				t0 ? t0( ev ) : 0;
 			} );
 			return r
-		})( dk.sList, dkEvent ) ),
+		})( dk.sList, dk.addEvent, dkEvent ) ),
 
 		dk.obj( 'WIN', (function( $detector ){
 			var r = {
