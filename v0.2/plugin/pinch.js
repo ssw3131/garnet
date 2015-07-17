0feed14a1be83b224bc62768b7261c01b519cdf1
sewon <ssw3131@daum.net>
2015-07-17 14:49:18 +0900

    dk v0.2.1 pinch

--- v0.2/plugin/pinch.js
+++ v0.2/plugin/pinch.js
@@ -15,7 +15,7 @@ dk.obj( 'PINCH', (function( $sList, $dkMouse, $dkEvent ){
 		init = function( $ev ){ $ev.nativeEvent.stopPropagation(), $ev.nativeEvent.preventDefault(), $ev.type = 'oneStart', startFlag = true, initFlag = true, reset( $ev ); },
 
 		func = function( $e ){
-			log( count++ )
+			log( 'p' + count++ )
 			return;
 			var ev = $dkEvent( $e.nativeEvent ), touchList = $dkMouse.touches, leng = touchList.length, point0, point1;
 
