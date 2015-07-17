84aebb3456ca6d5eb650118c14346072675f845f
sewon <ssw3131@daum.net>
2015-07-17 16:46:22 +0900

    dk v0.2.1 pinch

--- v0.2/plugin/pinch.js
+++ v0.2/plugin/pinch.js
@@ -12,28 +12,26 @@
  */
 dk.obj( 'PINCH', (function( $sList, $dkMouse, $dkEvent ){
 	var r, func, start, end, mPow = Math.pow, mSqrt = Math.sqrt, mMax = Math.max, mMin = Math.min, mAbs = Math.abs,
-		getDistance, reset, startFlag = false, initFlag = false, oldScale, initD, init, pinchFlag = false, initOne = {}, initPinch = {}, count = 0;
+		getDistance, reset, initFlag = false, oldScale, initD, init, pinchFlag = false, initOne = {}, initPinch = {}, count = 0;
 
 	getDistance = function( $p0, $p1 ){ return ~~mSqrt( mPow( $p0.x - $p1.x, 2 ) + mPow( $p0.y - $p1.y, 2 ) ); },
 		reset = function( $ev ){ $ev.distance = 0, $ev.moveX = 0, $ev.moveY = 0, $ev.scale = 1, oldScale = 1, $ev.centerX = 0, $ev.centerY = 0; },
-		init = function( $ev ){ $ev.type = 'oneStart', startFlag = true, initFlag = true, reset( $ev ); },
+		init = function( $ev ){ $ev.type = 'oneStart', initFlag = true, reset( $ev ); },
 
 		func = function( $e ){
 			var ev = $dkEvent( $e.nativeEvent ), touchList = $dkMouse.touches, leng = touchList.length, point0, point1;
 
 				point0 = { x : touchList[ 0 ].pageX, y : touchList[ 0 ].pageY };
 
-			if( leng != 0 ) $e.nativeEvent.stopPropagation(), $e.nativeEvent.preventDefault();
+			$e.nativeEvent.stopPropagation(), $e.nativeEvent.preventDefault();
 
-			if( leng == 0 ){
-				ev.type = 'oneEnd', startFlag = false, initFlag = false, reset( ev ), r[ 'update' ]( ev ); // oneEnd
-			}else if( leng == 1 ){
+			if( leng == 1 ){
 				if( $e.type == 'move' ){
-					if( startFlag ) ev.type = 'one', ev.scale = oldScale, ev.moveX = point0.x - initOne.x, ev.moveY = point0.y - initOne.y, r[ 'update' ]( ev ); // one
+					if( initFlag ) ev.type = 'one', ev.scale = oldScale, ev.moveX = point0.x - initOne.x, ev.moveY = point0.y - initOne.y, r[ 'update' ]( ev ); // one
 					else init( ev ), initOne = point0, r[ 'update' ]( ev ); // oneStart one
 				}else if( $e.type == 'up' ){
 					if( pinchFlag ) ev.type = 'pinchEnd', initOne = point0, ev.moveX = 0, ev.moveY = 0, r[ 'update' ]( ev ); // pinchEnd
-					else ev.type = 'oneEnd', startFlag = false, initFlag = false, reset( ev ), r[ 'update' ]( ev ); // oneEnd
+					else ev.type = 'oneEnd', initFlag = false, reset( ev ), r[ 'update' ]( ev ); // oneEnd
 				}
 				pinchFlag = false;
 			}else if( leng > 1 ){
