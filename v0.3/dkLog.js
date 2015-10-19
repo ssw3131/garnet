19a05b3ea15121e1b071c0b6a5fb8efb7e749fbd
sewon <ssw3131@daum.net>
2015-10-19 16:15:13 +0900

    dk garnet v0.3

--- v0.3/dkLog.js
+++ v0.3/dkLog.js
@@ -1,11 +1,9 @@
 (function(){
     var W = window, DOC = document;
-    var log, logArr = [], e0, e1, es0, es1, toggle, x = 710, y = 10, w = 800;
+    var log, logArr = [], e0, es0, toggle;
 
     e0 = DOC.createElement( "div" ), es0 = e0.style,
-        es0.cssText = "left : " + x + "px; top : " + ( y + 22 ) + "px; width : " + w + "px; height : 90%; position : fixed; display : block; overflow : auto; padding : 10px; background-color : #000; font : 12px/18px 돋움, sans-serif; color : #FFF; opacity : 0.8; z-index : 10000000;",
-        e1 = DOC.createElement( "div" ), es1 = e1.style,
-        es1.cssText = "left : " + x + "px; top : " + y + "px; width : " + ( w + 10 ) + "px; height : 20px; position : fixed;display : block; padding-left : 10px; background-color : #000; font : 12px/18px 돋움, sans-serif; color : #FFF; opacity : 0.8; z-index : 10000000;",
+        es0.cssText = "left : 700px; top : 10px; width : 800px; height : 90%; position : fixed; display : block; overflow : auto; padding : 10px; background-color : #000; font : 12px/18px 돋움; color : #FFF; opacity : 0.8; z-index : 10000000;",
 
         W.log = log = (function(){
             if( W[ "console" ] !== undefined )
@@ -22,19 +20,20 @@
             var jcJoin = Array.prototype.join;
             return function(){
                 var body = DOC.body;
-                if( body ) e0.innerHTML = jcJoin.call( logArr, "<br>" ), body.appendChild( e0 ), body.appendChild( e1 ), toggle = true;
+                if( body ) e0.innerHTML = jcJoin.call( logArr, "<br>" ), body.appendChild( e0 ), toggle = true;
             }
         })(),
 
         log.hide = function(){
             var body = DOC.body;
-            if( body ) body.removeChild( e0 ), body.removeChild( e1 ), toggle = false;
+            if( body ) body.removeChild( e0 ), toggle = false;
         },
 
         log.position = function( $prop ){
-            if( $prop.left !== undefined ) es0.left = $prop.left + "px", es1.left = $prop.left + "px";
-            if( $prop.top !== undefined ) es0.top = $prop.top + 22 + "px", es1.top = $prop.top + "px";
-            if( $prop.width !== undefined ) es0.width = $prop.width + "px", es1.width = $prop.width + 10 + "px";
+            if( $prop.left !== undefined ) es0.left = $prop.left + "px";
+            if( $prop.top !== undefined ) es0.top = $prop.top + "px";
+            if( $prop.width !== undefined ) es0.width = $prop.width + "px";
+            if( $prop.height !== undefined ) es0.height = $prop.height + "px";
         },
 
         log.show();
